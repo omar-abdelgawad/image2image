e6fe55eaa714e55a65d80876239d773f13f70469
omar-abdelgawad <omar.abdelgawad@ejust.edu.eg>
2023-12-31 04:04:04 +0200

    Fixed pytest-cov

--- tests/test_build.py
+++ tests/test_build.py
@@ -1,8 +1,12 @@
 """Test module for making sure model package is working"""
 import pix2pix
+import tunit
+import unit
 
 
 def test_build():
     """Make sure pytest is working."""
     assert True
     assert pix2pix
+    assert tunit
+    assert unit
