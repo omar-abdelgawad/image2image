5c85865c5569f1c8c57e8d7dd2fa799fdd78cf3d
eyad-hussein <eyad.hussein@ejust.edu.eg>
2024-05-08 13:20:29 +0300

    eyad

--- api/app/api.py
+++ api/app/api.py
@@ -1,9 +1,13 @@
 from flask import Blueprint, request, jsonify
+from flask_cors import CORS  # Import CORS from flask_cors
 from PIL import Image
 import os
 from .image_processing import process_image
 
 api_blueprint = Blueprint("api", __name__)
+
+
+CORS(api_blueprint)
 
 
 @api_blueprint.route("/coloring", methods=["POST"])
@@ -23,4 +27,4 @@ def process_image_route():
         "/home/eyad/watashi-ubuntu/academics/seminar/pbl/pbl-front/public/img.png"
     )
     # return processed_image
-    # return jsonify({"path": abs_path})
+    return jsonify({"path": abs_path})
